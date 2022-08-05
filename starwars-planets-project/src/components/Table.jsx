1c3ca2a86811913c3152ef6e6997f66a22713c09
Fernanda Grein <fernandagrein.fgm@outlook.com>
2022-08-05 11:40:25 -0300

    adiciona componente Table

---
+++ starwars-planets-project/src/components/Table.jsx
@@ -0,0 +1,48 @@
+import React, { useContext } from 'react';
+import PlanetContex from '../contex/PlanteContex';
+
+function Table() {
+  const { filteredPlanets } = useContext(PlanetContex);
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Rotation Period</th>
+          <th>Orbital Period</th>
+          <th>Diameter</th>
+          <th>Climate</th>
+          <th>Gravity</th>
+          <th>Terrain</th>
+          <th>Surface Water</th>
+          <th>Population</th>
+          <th>Films</th>
+          <th>created</th>
+          <th>edited</th>
+          <th>url</th>
+        </tr>
+      </thead>
+      <tbody>
+        {filteredPlanets.map((item, index) => (
+          <tr aria-label="row" key={ index }>
+            <td>{item.name}</td>
+            <td>{item.rotation_period}</td>
+            <td>{item.orbital_period}</td>
+            <td>{item.diameter}</td>
+            <td>{item.climate}</td>
+            <td>{item.gravity}</td>
+            <td>{item.terrain}</td>
+            <td>{item.surface_water}</td>
+            <td>{item.population}</td>
+            <td>{item.films}</td>
+            <td>{item.created}</td>
+            <td>{item.edited}</td>
+            <td>{item.url}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default Table;
